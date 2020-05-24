c6137b3030c92bbde02658d8cfdf2f6436d650c8
joe <lau.joe.a@gmail.com>
2020-05-24 12:47:08 -0700

    formatted userlist

--- src/features/userList/UserList.jsx
+++ src/features/userList/UserList.jsx
@@ -1,52 +1,49 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import { unwrapResult } from '@reduxjs/toolkit';
+import React, {useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUserById } from './userListSlice';
 
-import {
-    fetchUserById,
-} from './userListSlice';
-import { log_json } from '../utils';
 
 export function UserList() {
-    // const count = useSelector(selectCount);
-    useSelector((state) => {
-        console.log(`use selector - state:\n${JSON.stringify(state, null, 2)}`);
-        return state.users;
-    });
-    const dispatch = useDispatch();
+  // const count = useSelector(selectCount);
+  useSelector((state) => {
+    console.log(`use selector - state:\n${JSON.stringify(state, null, 2)}`);
+    return state.users;
+  });
+  const dispatch = useDispatch();
 
-    const fetchOneUser = async (userId) => {
-        try {
-            const resultAction = await dispatch(fetchUserById(userId));
-            const user = unwrapResult(resultAction);
-            // showToast('success', `Fetched ${user.name}`)
-            console.log(`Fetched ${user}`);
-            return (
-                <div>
-                    <h1>
-                        User:
+  const fetchOneUser = async (userId) => {
+    try {
+      const resultAction = await dispatch(fetchUserById(userId));
+      const user = unwrapResult(resultAction);
+      // showToast('success', `Fetched ${user.name}`)
+      console.log(`Fetched ${user}`);
+      return (
+        <div>
+          <h1>
+            User:
                         {user}
-                    </h1>
+          </h1>
 
-                </div>
-            );
-            // return user;
-        } catch (err) {
-            // showToast('error', `Fetch failed: ${err.message}`)
-            console.error(`Fetch failed: ${err.message}`);
-            // return err;
-            return (
-                <div>
-                    <h1>
-                        Error:
+        </div>
+      );
+      // return user;
+    } catch (err) {
+      // showToast('error', `Fetch failed: ${err.message}`)
+      console.error(`Fetch failed: ${err.message}`);
+      // return err;
+      return (
+        <div>
+          <h1>
+            Error:
                         {err}
-                    </h1>
+          </h1>
 
-                </div>
-            );
-        }
-    };
-    const userId = useSelector((state) => state.counter.value);
-    return fetchOneUser(userId);
+        </div>
+      );
+    }
+  };
+  const userId = useSelector((state) => state.counter.value);
+  return await fetchOneUser(userId);
 
 }
